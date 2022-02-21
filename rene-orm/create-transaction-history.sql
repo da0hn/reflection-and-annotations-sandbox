970f0efbbec10cd92a4d8660aeb950c2cdf43e02
da0hn <gabrielhondacba@gmail.com>
2022-02-21 10:03:11 -0400

    implements launch of H2 database

---
+++ rene-orm/create-transaction-history.sql
@@ -0,0 +1,9 @@
+CREATE TABLE transaction_history
+(
+    transactionId int,
+    accountNumber int,
+    name varchar(50),
+    transactionType varchar(12),
+    amount double
+);
+
--- rene-orm/src/main/java/org/gabriel/sandbox/rene/H2Launcher.java
+++ rene-orm/src/main/java/org/gabriel/sandbox/rene/H2Launcher.java
@@ -2,10 +2,22 @@ package org.gabriel.sandbox.rene;
 
 import org.h2.tools.Server;
 
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class H2Launcher {
-  public static void main(final String[] args) throws SQLException {
-    Server.main(args);
+  public static void main(final String[] args) throws SQLException, ClassNotFoundException {
+    //createDatabase();
+    Server.main("-ifNotExists");
+  }
+
+  private static void createDatabase() throws ClassNotFoundException, SQLException {
+    Class.forName("org.h2.Driver");
+    final var connection = DriverManager.getConnection(
+      "jdbc:h2:~/external_hd/code/reflection-and-annotations-sandbox/rene-orm/h2_database/any_database",
+      "sa",
+      ""
+    );
+    System.out.println("Database created!");
   }
 }
