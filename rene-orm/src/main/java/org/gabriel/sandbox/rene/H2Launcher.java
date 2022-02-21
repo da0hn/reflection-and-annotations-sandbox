9d1f58f9845f9621f19d285ee3cdf4d64fca7042
da0hn <gabrielhondacba@gmail.com>
2022-02-21 08:43:42 -0400

    add `H2` embedded database launcher

---
+++ rene-orm/src/main/java/org/gabriel/sandbox/rene/H2Launcher.java
@@ -0,0 +1,11 @@
+package org.gabriel.sandbox.rene;
+
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
+
+public class H2Launcher {
+  public static void main(final String[] args) throws SQLException {
+    Server.main(args);
+  }
+}
